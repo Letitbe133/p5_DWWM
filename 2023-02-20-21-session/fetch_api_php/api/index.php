08eecf99b59960248dabf9c765777f346c641a6e
letitbe133 <lionel.lft@me.com>
2023-02-22 19:53:49 +0100

    [UPDATE] mise à jour commentaires dans la démo Fetch

--- 2023-02-20-21-session/fetch_api_php/api/index.php
+++ 2023-02-20-21-session/fetch_api_php/api/index.php
@@ -1,1 +1,19 @@
 <?php
+
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
+
+$name = $_GET['name'];
+
+if (!empty($name)) {
+    $response = [
+        'status' => true,
+        'name' => $name,
+    ];
+} else {
+    $response = [
+        'status' => false,
+        'name' => 'Anonymous',
+    ];
+}
+
+echo json_encode($response);
--- 2023-02-20-21-session/fetch_api_php/js/app.js
+++ 2023-02-20-21-session/fetch_api_php/js/app.js
@@ -1,45 +1,65 @@
-// https://jsonplaceholder.typicode.com/todos
+// // https://jsonplaceholder.typicode.com/todos
 
-// appel fetch classique avec chaînage .then
+// // appel fetch classique avec chaînage .then
 
-// récup de la ressource
-// fetch("https://jsonplaceholder.typicode.com/todos", {
-//   method: "GET",
-//   headers: {
-//     Accept: "application/json",
-//   },
-// })
-//   .then(function (response) {
-//     // console.log(response);
-//     return response.json();
-//   })
-//   .then(function (todos) {
-//     // console.table(data);
+// // récup de la ressource
+// // fetch("https://jsonplaceholder.typicode.com/todos", {
+// //   method: "GET",
+// //   headers: {
+// //     Accept: "application/json",
+// //   },
+// // })
+// //   .then(function (response) {
+// //     // console.log(response);
+// //     return response.json();
+// //   })
+// //   .then(function (todos) {
+// //     // console.table(data);
 
-//     // data contient un tableau d'objets
-//     // on veut récupérer tous les todos dont la prop completed est à false
+// //     // data contient un tableau d'objets
+// //     // on veut récupérer tous les todos dont la prop completed est à false
 
-//     const notCompletedTodos = todos.filter(function (todo) {
-//       return todo.completed === false;
-//     });
+// //     const notCompletedTodos = todos.filter(function (todo) {
+// //       return todo.completed === false;
+// //     });
 
-//     console.table(notCompletedTodos);
-//   });
+// //     console.table(notCompletedTodos);
+// //   });
 
-// utilisation de async / await
-// on déclare une fonction getTodos qui va être asynchrone
-async function getTodos() {
-  // à l'intérieur on fait notre fetch, puis on attend la réponse
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  //   console.log(response);
+// // utilisation de async / await
+// // on déclare une fonction getTodos qui va être asynchrone
+// async function getTodos() {
+//   // à l'intérieur on fait notre fetch, puis on attend la réponse
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   //   console.log(response);
 
-  //   une fois la réponse reçue, on peut la traiter et récupérer les données
-  const todos = await response.json();
+//   //   une fois la réponse reçue, on peut la traiter et récupérer les données
+//   const todos = await response.json();
 
-  console.table(todos);
-}
+//   console.table(todos);
+// }
 
-// ici on appelle la fonction
-getTodos();
+// // ici on appelle la fonction
+// getTodos();
 
-// TODOS : ajouter la gestion d'erreurs éventuelles
+// // TODOS : ajouter la gestion d'erreurs éventuelles
+
+// async function getNews() {
+//   try {
+//     const res = await fetch("");
+
+//     if (!res.ok) throw new Error("Error occured : " + res.statusText);
+
+//     const data = await res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+fetch("http://localhost:8000/index.php?name=Lionel")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
